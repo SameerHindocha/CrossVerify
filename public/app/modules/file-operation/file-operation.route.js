669e79ebef82523fb737f848181b2d26a0fe78d5
Sameer Hindocha <sameer.hindocha@rapidops.com>
2017-08-11 19:24:44 +0530

    Compare

--- public/app/modules/file-operation/file-operation.route.js
+++ public/app/modules/file-operation/file-operation.route.js
@@ -4,7 +4,7 @@
     .module('fileApp')
     .config(['$routeProvider', function($routeProvider) {
       $routeProvider
-        .when("/file-compare/:id", {
+        .when("/file-compare/:id/:gst", {
           templateUrl: 'app/modules/file-operation/view/file-comparision.html',
           controller: 'fileComparisionController',
           controllerAs: 'vm',
